e27846037450cda9760eade081d035fd066b45cf
ignacio <ign.garcia61@gmail.com>
2024-10-23 19:58:11 -0300

    feat: add match and team relationship

--- src/Models/Match.cs
+++ src/Models/Match.cs
@@ -1,13 +1,19 @@
-namespace Models
-{
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace worlds_tracker.src.Models
+{
     public class Match
     {
         public int Id { get; set; }
-        public DateTime Date { get; set; }
-        public Team Team1 { get; set; }
-        public Team Team2 { get; set; }
-        public string Result { get; set; }
+        public DateTime? Date { get; set; }
+        public int? Team1Id { get; set; }
+        public int? Team2Id { get; set; }
+        public Team? Team1 { get; set; }
+        public Team? Team2 { get; set; }
+        public string Result { get; set; } = string.Empty;
     }
 
 }
--- src/Models/Team.cs
+++ src/Models/Team.cs
@@ -1,10 +1,16 @@
-namespace Models
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace worlds_tracker.src.Models
 {
     public class Team
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Region { get; set; }
-        public int Points { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Region { get; set; } = string.Empty;
+        public int Points { get; set; } = 0;
+        public List<Match> Matches { get; set; } = new List<Match>();
     }
 }
